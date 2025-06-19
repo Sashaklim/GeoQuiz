8ef2c7e2ac5b5bebee8072b15672bdf8eb2360c5
Sashaklim <sashaklimovic995@gmail.com>
2025-06-19 23:27:22 +0300

    2. Описание структуры сайта

--- app/src/main/java/com/example/geoquiz41/MainActivity.kt
+++ app/src/main/java/com/example/geoquiz41/MainActivity.kt
@@ -1,20 +1,21 @@
 package com.example.geoquiz41
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.Button
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+
+    private lateinit var trueButton: Button
+    private lateinit var falseButton: Button
+    private lateinit var nextButton: Button
+    private lateinit var questionTextView: TextView
+
+    private var currentIndex = 0
+    private var score = 0
+    private lateinit var answeredQuestions: BooleanArray
         }
-    }
-}
+
